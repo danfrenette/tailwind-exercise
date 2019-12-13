7f8a10d02e5869b2cd5bfae2983c0b0591fc92c5
danfrenette <dan.r.frenette@gmail.com>
2019-12-12 21:47:34 -0500

    kind of spaced tabs...also hex

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="flex bg-cover h-screen" style={{backgroundImage: "url('/2btestwallpaper.jpeg')"}}>
       <div className="w-5/12 flex flex-col items-center justify-center">
         <div className="block w-6/12">
+          {/* <div className="hexagon"></div> */}
           <img class="rounded-full border-solid border-white border-4" src="/headshot.jpg" />
         </div>
 
@@ -14,8 +15,24 @@ function App() {
         </div>
       </div>
 
-      <div className="w-7/12 h-full">
-        <div className="hexagon"></div>
+      <div className="w-7/12 flex flex-col items-center justify-center">
+        <nav className="bg-transparent w-4/5 flex justify-between">
+          <div className="inline-block w-1/3 border-solid border-white border-l-4 border-t-4 text-center ml-8">
+            <a href="#" class="px-2 py-1 h-10 block font-semibold text-white hover:bg-gray-300">test link</a>
+          </div>
+          <div className="inline-block w-1/3 border-solid border-white border-l-4 border-t-4 text-center ml-8">
+            <a href="#" class="px-2 py-1 h-10 block font-semibold text-white hover:bg-gray-300">test link</a>
+          </div>
+          <div className="inline-block w-1/3 border-solid border-white border-l-4 border-t-4 text-center ml-8">
+            <a href="#" class="px-2 py-1 h-10 block font-semibold text-white hover:bg-gray-300">test link</a>
+          </div>
+        </nav>
+
+        <div className="mt-8 w-4/5 flex justify-between">
+          <div className="bg-gray-600 inline-block border-solid border-white ml-8 w-full h-auto">
+            text
+          </div>
+        </div>
       </div>
     </div>
   );
