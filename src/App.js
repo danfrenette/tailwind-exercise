63e9d270f3994fb2d53681add5547b471ae6ea21
danfrenette <dan.r.frenette@gmail.com>
2020-01-01 13:50:23 -0500

    Add font awesome or something

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Tab from './Tab';
+
+const githubLink = "https://www.github.com/danfrenette"
+const linkedinLink = "https://www.linkedin.com/in/danfrenette/"
+const emailLink = "mailto: Dan.R.Frenette@gmail.com"
 
 function App() {
   return (
@@ -8,11 +14,20 @@ function App() {
       <div className="w-5/12 flex flex-col items-center justify-center">
         <div className="block w-6/12">
           {/* <div className="hexagon"></div> */}
-          <img class="rounded-full border-solid border-white border-4" src="/headshot.jpg" />
+          <img className="rounded-full border-solid border-white border-4" src="/headshot.jpg" />
         </div>
 
-        <div className="block w-6/12 text-center mt-8">
-          <h1 className="text-6xl text-white">Dan Frenette</h1>
+        <div className="block w-6/12 text-center mt-8 text-white">
+          <h1 className="text-6xl">Dan Frenette</h1>
+          <a href={githubLink}>
+            <FontAwesomeIcon className="mx-3 text-4xl" icon={faGithub} />
+          </a>
+          <a href={linkedinLink}>
+            <FontAwesomeIcon className="mx-3 text-4xl" icon={faLinkedin} />
+          </a>
+          <a href={emailLink}>
+            <FontAwesomeIcon className="mx-3 text-4xl" icon={faEnvelope} />
+          </a>
         </div>
       </div>
 
